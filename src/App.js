e0a4948066ee1111e4be54c679e0554acec26634
BearKing <saurabhsaini228@gmail.com>
2023-12-30 18:48:58 +0530

    Corrects GenderYearGraph Buttons Colors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 import './App.css';
-import Sidebar from './Components/Sidebar';
+import Sidebar from './components/Sidebar';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Home from './Components/Home';
-import Age from './Components/Age';
-import Gender from './Components/Gender';
-import Caste from './Components/Caste';
-import School from './Components/School';
-import Region from './Components/Region';
-import CastAnalysis from './Components/CastAnalysis';
+import Home from './components/Home';
+import Age from './components/Age';
+import Gender from './components/Gender';
+import Caste from './components/Caste';
+import School from './components/School';
+import Region from './components/Region';
+import CastAnalysis from './components/CastAnalysis';
 
 import { AnimatePresence } from 'framer-motion';
-import Loader from './Components/loader';
+import Loader from './components/loader';
 
 function App() {
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 // // Routes.js
 // import React from 'react';
 // import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-// // import Visuals from './Components/Visuals';
-// import Home from './Components/Home';
-// import Age from './Components/Age';
-// import Gender from './Components/Gender';
-// import Caste from './Components/Caste';
-// import School from './Components/School';
-// import Region from './Components/Region';
-// import CastAnalysis from './Components/CastAnalysis';
+// // import Visuals from './components/Visuals';
+// import Home from './components/Home';
+// import Age from './components/Age';
+// import Gender from './components/Gender';
+// import Caste from './components/Caste';
+// import School from './components/School';
+// import Region from './components/Region';
+// import CastAnalysis from './components/CastAnalysis';
 
 // const AppRoutes = () => {
 
